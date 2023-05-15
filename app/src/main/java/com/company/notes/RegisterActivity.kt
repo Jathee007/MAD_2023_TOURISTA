5c31b48a6706c38c5d9e0bf68a89bfea69bb671a
Gishor04 <rajangishor04@gmail.com>
2023-05-15 11:52:43 +0530

    add register part

--- app/src/main/java/com/company/notes/RegisterActivity.kt
+++ app/src/main/java/com/company/notes/RegisterActivity.kt
@@ -14,7 +14,7 @@ import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
-
+//register part
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
